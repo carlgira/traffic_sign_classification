e8a9c4bccfd93d2bebdfa11398641b21ad908246
carlgira <carlgira@gmail.com>
2017-11-04 13:58:20 +0000

    tunning

--- src/lenet_basic/lenet_basic.py
+++ src/lenet_basic/lenet_basic.py
@@ -10,22 +10,20 @@ class LeNETBasic(tsf.TrafficSignClassifier):
 
 	# Neural Network
 	def neural_network(self, x_data, y_data, phase):
-		conv1 = tf.layers.conv2d(x_data, filters=6, kernel_size=[1, 1], padding='valid', activation=tf.nn.relu, kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.1))
-		conv1 = tf.layers.max_pooling2d(conv1, pool_size=[2, 2], strides=2)
+		conv1 = tf.layers.conv2d(x_data, filters=6, kernel_size=[5, 5], padding='valid', activation=tf.nn.relu, kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.1))
+		maxp1 = tf.layers.max_pooling2d(conv1, pool_size=[2, 2], strides=2)
 
-		conv2 = tf.layers.conv2d(conv1, filters=16, kernel_size=[1, 1], padding='valid', activation=tf.nn.relu, kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.1))
-		conv2 = tf.layers.max_pooling2d(conv2, pool_size=[2, 2], strides=2)
+		conv2 = tf.layers.conv2d(maxp1, filters=16, kernel_size=[5, 5], padding='valid', activation=tf.nn.relu, kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.1))
+		maxp2 = tf.layers.max_pooling2d(conv2, pool_size=[2, 2], strides=2)
 
-		fc0 = flatten(conv2)
+		fc0 = flatten(maxp2)
 		fc1 = tf.layers.dense(fc0, units=120, activation=tf.nn.relu)
 
 		fc2 = tf.layers.dense(fc1, units=84, activation=tf.nn.relu)
 
 		logits = tf.layers.dense(fc2, units=self.classes)
 
-		output = tf.nn.softmax(logits)
-
-		accuracy_operation = tf.reduce_mean(tf.cast(tf.equal(tf.arg_max(y_data, 1 ), tf.arg_max(output, 1)), tf.float32), name='accuracy')
+		accuracy_operation = tf.reduce_mean(tf.cast(tf.equal(tf.arg_max(y_data, 1 ), tf.arg_max(logits, 1)), tf.float32), name='accuracy')
 		loss_operation = tf.losses.softmax_cross_entropy(onehot_labels=y_data, logits=logits)
 		training_operation = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(loss_operation)
 
